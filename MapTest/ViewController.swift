82e63d17ea301a5d2d6afc23bae6dcd4fde8fd7f
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-09-04 14:04:56 -0400

    Add Map Kit view to the default scene.

--- MapTest/ViewController.swift
+++ MapTest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     // MARK: Outlets
     
     @IBOutlet var mapModeSegmentControl: UIView!
+    @IBOutlet weak var mapView: MKMapView!
     
     
     // MARK: Action Methods
